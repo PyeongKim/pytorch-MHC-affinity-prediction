a2d13ab13a604836e00e15fd05a440dff8f89963
Pyeong Eun Kim <35253323+PyeongKim@users.noreply.github.com>
2020-05-11 18:48:57 +0900

    Update helper_module.py

--- src/helper_module.py
+++ src/helper_module.py
@@ -1,3 +1,17 @@
+def set_target_dic(df_path_1, df_path_2):
+  df_1 = pd.read_csv(df_path_1)
+  df_2 = pd.read_csv(df_path_2)
+  data_target_1 = df_1.iloc[:,1]
+  data_target_2 = df_2.iloc[:,1]
+  unique_targets_1 = list(set(data_target_1))
+  unique_targets_2 = list(set(data_target_2))
+  unique_targets = set(unique_targets_1+ unique_targets_2)
+  target_dic = {}
+  for n, target in enumerate(unique_targets):
+    target_dic[target] = n
+  return target_dic
+
+
 def train_the_model(dataloader, model, criterion, optimizer, epoch):
   loss_sum = 0
   for batch, (peptide, label, target) in enumerate(dataloader):
